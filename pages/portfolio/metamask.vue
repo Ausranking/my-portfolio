d42e02ebc6ba7a7ec3433bac111fa5240fdfe4fe
Austin Nze <austinnze1000@gmail.com>
2023-09-26 14:57:21 +0100

    updating projects

--- pages/portfolio/metamask.vue
+++ pages/portfolio/metamask.vue
@@ -6,13 +6,13 @@
 
 <script>
 export default {
-  name: "AlaajoPage",
+  name: "MetamaskPage",
   layout: "portfolio",
   data() {
     return {
       project: {
         description: [
-          "Responsible for building 80% of the front end with minimal supervision",
+          "Responsible for building 90% of the front end with minimal supervision",
           "Connected the web app to external fintech APIs like Stripe and Mono for various features such as Account name verification, Credit Score Checks, and Card & Web Payments.",
           "Designed the platform to handle a large number of users and transactions, but it was important to ensure that the platform could handle the load without any downtime or performance issues.",
           "Implemented a custom authentication system using JWT and Redis to ensure that the platform was secure and could handle a large number of users.",
